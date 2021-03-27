69e3cc24cecb67f9d751a76ca7d48b74cd08320e
burswyck <ibkov@yandex.ru>
2021-03-27 21:20:46 +0300

    123

--- cube3d.c
+++ cube3d.c
@@ -1,13 +1,28 @@
 # include "includes/cube3d.h"
 
+void run_game(int cr_bmp, char namefile, t_all *all)
+{
+    all->win.mlx_ptr = mlx_init();
+}
+
+void init_struct(int cr_bmp, char *namefile)
+{
+  t_all all;
+  ft_bzero(&all, sizeof(t_all));
+  ft_bzero(&all.win, sizeof(t_win));
+  ft_bzero(&all.img, sizeof(t_img));
+  ft_bzero(&all.tex, sizeof(t_tex));
+  ft_bzero(&all.game, sizeof(t_game));
+  run_game(cr_bmp, namefile, &all);
+}
 
 int main(int argc, char **argv)
 {
     if (argc == 3 && !ft_strncmp(argv[1] + ft_strlen(argv[1]) - 4, ".cub", 4) && \
       !ft_strncmp(argv[2], "--save", 6))
-      ft_putstr_fd("ok\n", 1);
+      init_struct(1, av[1]);
     else if (argc == 2 && !ft_strncmp(argv[1] + ft_strlen(argv[1]) - 4, ".cub", 4))
-      ft_putstr_fd("ok\n", 1);
+      init_struct(0, av[1]);
     else
       ft_putstr_fd("\033[31m\nERROR\nMAIN ARGUMENTS\n\n\033[0m", 2);
 }
